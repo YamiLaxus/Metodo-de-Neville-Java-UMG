19c29586dc27e1099d21395302024c3649c470e0
YamiLaxus <osbaldoez@hotmail.com>
2023-05-27 13:36:53 -0600

    Algoritmo completo,
    TODO
    Mejorar interfaz y mostrar todos los datos.

--- src/main/java/com/osdev/nevillemethod/NevilleMethod.java
+++ src/main/java/com/osdev/nevillemethod/NevilleMethod.java
@@ -15,29 +15,5 @@ public class NevilleMethod {
         mf.setVisible(true);
         mf.setLocationRelativeTo(null);
 
-        double[] x = {1.0, 2.0, 4.0};
-        double[] y = {3.0, 5.0, 7.0};
-        double p = 3.0;
-
-        double result = Neville(x, y, p);
-//        System.out.println(result);
-
-    }
-
-    public static double Neville(double[] x, double[] y, double p) {
-        int n = x.length;
-        double[][] q = new double[n][n];
-
-        for (int i = 0; i < n; i++) {
-            q[i][0] = y[i];
-        }
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j <= i; j++) {
-                q[i][j] = ((p - x[i - j]) * q[i][j - 1] - (p - x[i]) * q[i - 1][j - 1]) / (x[i] - x[i - j]);
-            }
-        }
-
-        return q[n - 1][n - 1];
     }
 }
